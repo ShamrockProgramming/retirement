c9ae3410093da6d3f1fd4e7171c0340286753d32
Mr. Fleming <efleming@stpatrick.org>
2017-08-31 09:19:55 -0500

    started working

---
+++ retirement.js
@@ -0,0 +1,32 @@
+//Function declarations
+function retire(age){
+    let year; //set current year
+    let retirementAge; //set current retirement age
+    let diff = retirementAge - age;
+
+    //write three separate conditional statements to say when you can take out money.
+
+    //first if your age is older than the retirementAge
+
+    //second if your age is equal to the retirementAge
+
+    //third if your age is younger than the retirementAge
+
+}
+
+
+//main function tests your code above with different inputs.
+function main(){
+    retire(18);
+    retire(50);
+    retire(65);
+    retire(100); //Hundo!
+
+}
+
+
+
+
+// ctrl + shift + B
+//This will run the program.
+main();
